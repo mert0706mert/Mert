12ee1cd7428caf4e05bd0f627375735a35b918dd
MERT ÇETİNKAYA <mert-cetinkaya@outlook.com>
2021-04-28 19:11:52 +0300

    C#

--- Mert/Program.cs
+++ Mert/Program.cs
@@ -6,9 +6,34 @@ namespace Mert
     {
         static void Main(string[] args)
         {
-            MyList<string> sehir = new MyList<string>();
-            sehir.Add("Ankara");
-            Console.WriteLine(sehir.Count);
+            MyList<string> sehirler = new MyList<string>();
+            sehirler.Add("Ankara");
+            sehirler.Add("Antalya");
+            sehirler.Add("Ardahan");
+            sehirler.Add("Afyon");
+            sehirler.Add("Antep");
+            sehirler.Add("Adana");
+            sehirler.Add("Antakya");
+            sehirler.Add("Aydın");
+            sehirler.Add("Artvin");
+            sehirler.Add("Amasya");
+            sehirler.Add("Ağrı");
+            sehirler.Add("Aksaray");
+            sehirler.Add("Adıyaman");
+            Console.WriteLine("Türkiye' de " + sehirler.Count + " tane a ile başlayan il vardır");
+            Console.WriteLine("1)" + sehirler.Items[0]);
+            Console.WriteLine("2)" + sehirler.Items[1]);
+            Console.WriteLine("3)" + sehirler.Items[2]);
+            Console.WriteLine("4)" + sehirler.Items[3]);
+            Console.WriteLine("5)" + sehirler.Items[4]);
+            Console.WriteLine("6)" + sehirler.Items[5]);
+            Console.WriteLine("7)" + sehirler.Items[6]);
+            Console.WriteLine("8)" + sehirler.Items[7]);
+            Console.WriteLine("9)" + sehirler.Items[8]);
+            Console.WriteLine("10)" + sehirler.Items[9]);
+            Console.WriteLine("11)" + sehirler.Items[10]);
+            Console.WriteLine("12)" + sehirler.Items[11]);
+            Console.WriteLine("13)" + sehirler.Items[12] + "' dır");
         }
     }
     class MyList<T>
@@ -35,6 +60,9 @@ namespace Mert
         {
             get { return _array.Length; }
         }
-
+        public T[] Items
+        {
+            get { return _array; }
+        }
     }
 }
