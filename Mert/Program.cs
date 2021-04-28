b91319cae5c5ad376f33936ca8bc244048ed14ce
MERT ÇETİNKAYA <mert-cetinkaya@outlook.com>
2021-04-28 18:43:42 +0300

    Proje dosyası ekle.

---
+++ Mert/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Mert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> sehir = new MyList<string>();
+            sehirler.Add("Ankara");
+            Console.WriteLine(sehir.Count);
+        }
+    }
+    class MyList<T>
+    {
+        T[] _array;
+        T[] _tempArray;
+        public MyList()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for(int i=0;i<_tempArray.Length;i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+            _array[_array.Length - 1] = item;
+        }
+
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+
+    }
+}
